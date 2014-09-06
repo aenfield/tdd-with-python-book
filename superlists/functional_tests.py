fc4dfab9757df1a5190b8f20e68b2de8bfb40cb5
Andrew Enfield <aenfield@gmail.com>
2014-09-06 10:15:04 -0700

    First function test created, w/ comments. Also now uses unittest.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith retrieves the home page
+        self.browser.get('http://localhost:8000')
+
+        # notices page title and header, which both mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+        # invited to enter a to-do right away
+
+        # types 'buy peacock feathers' into a text box
+
+        # hits enter, the page updates, and the page now lists the item as a to-do
+
+        # the entry text box is still there, use it to enter a to-do to make a fly fishing lure
+
+        # the page updates again, and now shows both items
+
+        # will the page remember the list? oh, it's generated a unique URL for her and has text that explains this
+
+        # visit the URL - the list is still there
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
