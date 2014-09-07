ad77a152d53307fa2ce0692b3a53f45726d43088
Andrew Enfield <aenfield@gmail.com>
2014-09-07 10:22:36 -0700

    Move functional test from stand-alone file to app and update to use LiveServerTestCase for automatic DB creation/teardown. This also enables us to use manage.py test <appname> for all tests and run all tests at the same time.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith retrieves the home page
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # notices page title and header, which both mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -55,6 +56,3 @@ class NewVisitorTest(unittest.TestCase):
         # will the page remember the list? oh, it's generated a unique URL for her and has text that explains this
 
         # visit the URL - the list is still there
-        
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
