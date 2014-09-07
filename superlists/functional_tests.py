0d1ab9342c8c11fc079e06574975a1f149278fce
Andrew Enfield <aenfield@gmail.com>
2014-09-06 21:16:59 -0700

    Very basic HTML in template expanded and first few elements of the first functional test (start a new list and retrieve it later) working.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -36,7 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item didn't appear in table"
         )
 
         # the entry text box is still there, use it to enter a to-do to make a fly fishing lure
