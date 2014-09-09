ddac81479307b2e28db9fbde26c79419b29f60ca
Andrew Enfield <aenfield@gmail.com>
2014-09-09 08:38:51 -0700

    Implemented first cut of funct test to check that empty list items can't be submitted.

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -6,18 +6,29 @@ class ItemValidationTest(FunctionalTest):
            
     def test_cannot_add_empty_list_items(self):
         # Edith visits the home page and accidentally tries to submit an empty list item.
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         
         # The home page refreshes and shows an error message that says list items can't be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
         
         # So she enters text, and it works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
         
         # She tries again to create an empty list item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         
         # And it fails again in the same way
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
         
         # She fills in some text and it works
-        
-        self.fail("Not implemented yet.")
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
         
         
         
