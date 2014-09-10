6e678d2909f3392908b77b9db3f8a607f7b01491
Andrew Enfield <aenfield@gmail.com>
2014-09-09 20:17:57 -0700

    New list view now does - partially complete - model validation

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 #from django.http import HttpResponse
 
@@ -12,7 +13,13 @@ def view_list(request, list_id):
     
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        list_.delete()  # failed validation, so get rid of previously created list
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
     
 def add_item(request, list_id):
