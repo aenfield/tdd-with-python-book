3357951a5b4bae4e00603991d02175bf841a32d7
Andrew Enfield <aenfield@gmail.com>
2014-09-13 15:57:29 -0700

    Added nav bar with login link.

--- superlists/functional_tests/test_login.py
+++ superlists/functional_tests/test_login.py
@@ -7,7 +7,7 @@ class LoginTest(FunctionalTest):
     def test_login_with_persona(self):
         # Edith notices a Sign In link
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
         
         # a Persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -22,13 +22,13 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
         
         # she can see she's logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
         
 
     def switch_to_new_window(self, text_in_title):
-        retries = 60
+        retries = 20
         while retries > 0:
             for handle in self.browser.window_handles:
                 self.browser.switch_to_window(handle)
@@ -39,6 +39,6 @@ class LoginTest(FunctionalTest):
         self.fail('could not find window')
 
     def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
+        WebDriverWait(self.browser, timeout=10).until(
             lambda b: b.find_element_by_id(element_id)
         )
